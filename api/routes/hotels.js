4fb2043cc9a2aa93a04e166a65276ca72787bfa8
kkoahis <khoakun27@gmail.com>
2023-01-29 15:55:05 +0700

    creat first API

---
+++ api/routes/hotels.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const router = express.Router();
+
+// CREATE
+router.post("/", (req, res) => {
+   
+})
+// READ
+
+// UPDATE
+
+// DELETE
+
+// READALL
+
+export default router
