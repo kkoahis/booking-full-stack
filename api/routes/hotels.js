98e212c6fcfb6030d9d1e989bc42f251faa034cf
kkoahis <khoakun27@gmail.com>
2023-02-04 16:36:35 +0700

    done CRUD Hotels

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,17 +1,58 @@
 import express from "express";
+import Hotel from "../models/Hotel.js";
+import { creatError } from "../utils/error.js";
 
 const router = express.Router();
-
 // CREATE
-router.post("/", (req, res) => {
-   
+router.post("/", async (req, res) => {
+   const newHotel = new Hotel(req.body)
+
+   try {
+      const savedHotel = await newHotel.save()
+      res.status(200).json(savedHotel)
+   } catch (err) {
+      res.status(500).json(err);
+   }
 })
 // READ
-
+router.get("/:id", async (req, res) => {
+   try {
+      const hotel = await Hotel.findById(req.params.id)
+      res.status(200).json(hotel)
+   } catch (err) {
+      res.status(500).json(err);
+   }
+})
 // UPDATE
-
+router.put("/:id", async (req, res) => {
+   try {
+      const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+      res.status(200).json(updatedHotel)
+   } catch (err) {
+      res.status(500).json(err);
+   }
+})
 // DELETE
-
+router.delete("/:id", async (req, res) => {
+   try {
+      await Hotel.findByIdAndDelete(req.params.id);
+      res.status(200).json("Hotel has been deleted");
+   } catch (err) {
+      res.status(500).json(err);
+   }
+})
 // READALL
+router.get("/", async (req, res, next) => {
+   // const failed = true;
+   // if (failed)
+   //    return next(creatError(404, "You are not allowed to access this"));
+
+   try {
+      const hotels = await Hotel.find()
+      res.status(200).json(hotels)
+   } catch (err) {
+      next(err);
+   }
+})
 
 export default router
